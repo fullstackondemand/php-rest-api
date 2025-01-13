b1479cc46bd454cc6c864ffbfe6775e9a1c4ca4c
Pramod Singh Gusain <spramodgusain@gmail.com>
2025-01-13 20:20:45 +0530

    perf: store json type data

--- models/get.model.php
+++ models/get.model.php
@@ -1,26 +1,38 @@
-<?php 
+<?php
 
 /* GET Class */
 class GetModel {
-  
+
   function __construct() { }
 
   /* GET All Data */
   function getData($table, $id) {
-    
+
     /* Check ID */
     if(!isset($id)) { $where = ''; }
-    else { $where = "WHERE id = $id"; }
-    
+     else { $where = "WHERE id = $id"; }
+
     /* Include Database File */
     include 'views/database.view.php';
-    
+
     /* Get All Data */
     $sql = "SELECT * FROM  $table $where";
     $result = mysqli_query($con, $sql);
 
+    while ($row = mysqli_fetch_assoc($result)) {
+      $arr[] = $row;
+    }
+
+    foreach ($arr[0] as $key => $val) {
+
+      // String convert to Array
+      if (strpos($val, "[{") === 0) {
+        $arr[0][$key] = json_decode($val, true);
+      }
+    }
+
     if (mysqli_num_rows($result) > 0) {
-      echo json_encode(array('status'=>'Success', 'data'=>mysqli_fetch_all($result, MYSQLI_ASSOC)));
+      echo json_encode(array('status'=>'Success', 'data'=>$arr));
     } 
     else {
       echo json_encode(array('status'=>'Fail', 'error'=>'Please provide valid input.'));
--- models/put.model.php
+++ models/put.model.php
@@ -1,28 +1,34 @@
-<?php 
+<?php
 
 /* PUT Class */
 class PutModel {
-  
+
   function __construct() { }
 
   /* PUT Data */
   function putData($table, $id, $data) {
 
     /* Check Data and Check ID */
-    if(!isset($data) && !isset($id)) { 
-        echo json_encode(array('status'=>'Fail', 'error'=>'Please provide valid input.'));
-        die();
-     }
+    if(!isset($data) && !isset($id)) {
+      echo json_encode(array('status'=>'Fail', 'error'=>'Please provide valid input.'));
+      die();
+    }
 
     /* Include Database File */
     include 'views/database.view.php';
 
     /* Update Data */
     $data = json_decode($data, true);
-    $dataString = ''; 
+    $dataString = '';
 
     foreach($data as $key=>$value) {
-     $dataString = $dataString."$key = '$value', "; 
+
+      // Array convert to String
+      if (is_array($value)) {
+        $value = json_encode($value);
+      }
+
+      $dataString = $dataString."$key = '$value', ";
     }
 
     $dataString =  substr($dataString, 0, -2);
@@ -30,11 +36,11 @@ class PutModel {
     $sql = "UPDATE $table SET $dataString  WHERE id = $id";
 
     if ($con->query($sql) === TRUE) {
-            echo json_encode(array('status'=>'Success', 'message'=>'Data is Updated.'));
-    } 
+      echo json_encode(array('status'=>'Success', 'message'=>'Data is Updated.'));
+    }
     else {
-        echo json_encode(array('status'=>'Fail', 'error'=>'Please provide valid input.'));
-        die();
+      echo json_encode(array('status'=>'Fail', 'error'=>'Please provide valid input.'));
+      die();
     }
   }
 }
