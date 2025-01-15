9f6727d1ce780c961f846fc2662ade70401635b6
Pramod Singh Gusain <spramodgusain@gmail.com>
2025-01-15 18:57:22 +0530

    chore: wrong request handle

--- src/api/category/controller.php
+++ src/api/category/controller.php
@@ -1,6 +1,7 @@
 <?php
 declare(strict_types=1);
 namespace RestJS\Api\Category;
+use Slim\Exception\HttpBadRequestException;
 use RestJS\Api\Category\Model;
 use RestJS\Class\Response;
 use function RestJS\response;
@@ -18,12 +19,14 @@ class Controller {
     /** Fetch data by id */
     public function findById($req, $res, $args) {
         $result = $this->model->find( $args['id']);
+        count($result) == 0 && throw new HttpBadRequestException($req, "Something went wrong...");
         return response($req, $res, args: new Response(data: $result));
     }
 
     /** Delete data by id */
     public function deleteById($req, $res, $args) {
         $result = $this->model->delete($args['id']);
+        $result == 0 && throw new HttpBadRequestException($req, "Something went wrong...");
         return response($req, $res, new Response(message: "This item has been successfully removed.", data: $result));
     }
 
@@ -36,6 +39,7 @@ class Controller {
     /** Update by id */
     public function updateById($req, $res, $args) {
         $result = $this->model->update($req->getParsedBody(), $args["id"]);
+        $result == 0 && throw new HttpBadRequestException($req, "Somthing went wrong...");
         return response($req, $res, new Response(message: "This item has been successfully updated.", data: $result));
     }
 }
