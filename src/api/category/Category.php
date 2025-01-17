ca3dd180747bf02e0a15bd373e0507ab434e49bf
Pramod Singh Gusain <spramodgusain@gmail.com>
2025-01-17 19:33:54 +0530

    perf: entity use classes

--- src/api/category/Category.php
+++ src/api/category/Category.php
@@ -1,36 +1,35 @@
 <?php
 declare(strict_types=1);
 namespace RestJS\Api\Category;
-use Doctrine\DBAL\Schema\Table;
-use Doctrine\ORM\Mapping\Column;
-use Doctrine\ORM\Mapping\Entity;
-use Doctrine\ORM\Mapping\GeneratedValue;
-use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping as ORM;
 use RestJS\Trait\GetterAndSetter;
 
-#[Entity]
-#[Table('category')]
+#[ORM\Entity]
+#[ORM\Table('category')]
 class Category {
 
     // Get and Set methods
     use GetterAndSetter;
 
-    #[Id]
-    #[Column, GeneratedValue]
+    #[ORM\Id]
+    #[ORM\Column, ORM\GeneratedValue]
     private int $id;
 
-    #[Column(unique: true)]
-    private string $name;
+    #[ORM\Column(unique: true)]
+    private string $title;
 
-    #[Column(unique: true)]
+    #[ORM\Column(unique: true)]
     private string $slug;
 
-    #[Column(unique: true)]
+    #[ORM\Column]
     private string $description;
 
-    #[Column(name: "created_at", insertable: false, updatable: false)]
+    #[ORM\Column(name: "author_id")]
+    private int $authorId;
+
+    #[ORM\Column(name: "created_at", insertable: false, updatable: false)]
     private string $createdAt;
 
-    #[Column(name: "updated_at", insertable: false, updatable: false)]
+    #[ORM\Column(name: "updated_at", insertable: false, updatable: false)]
     private string $updatedAt;
 }
