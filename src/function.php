6cb9e6dd526caec2bb96257b5a4f3770bd0b7f89
Pramod Singh Gusain <spramodgusain@gmail.com>
2025-01-18 13:44:27 +0530

    chore: one-to-many and many-to-one relationship functions

--- src/function.php
+++ src/function.php
@@ -15,3 +15,38 @@ function response(Request $req, Response $res, mixed $args) {
 function checkNull($result, $req) {
     !boolval($result) && throw new HttpBadRequestException($req, "Something went wrong...");
 }
+
+/** Many To One Relationship Function */
+function ManyToOne($many, $one, $property) {
+    $result = [];
+
+    foreach ($many as $manyItem):
+        $manyItem[$property] = array_filter($one, fn($item) => $manyItem[$property] == $item['id'])[0];
+        array_push($result, $manyItem);
+    endforeach;
+
+    return $result;
+}
+
+/** One To Many Relationship Function */
+function OneToMany($one, $many, $property) {
+    $result = [];
+
+    foreach ($one as $oneItem):
+
+        if($oneItem[$property] == null) $oneItem[$property] =[];
+        else {
+            $ids = json_decode($oneItem[$property]);
+            $oneItem[$property] = [];
+
+            foreach ($ids as $id):
+                $array = array_filter($many, fn($item) => $id == $item['id']);
+                array_push($oneItem[$property], ...$array);
+            endforeach;
+        }
+        
+        array_push($result, $oneItem);
+    endforeach;
+
+    return $result;
+}
