b29191f9c7e7e54c785eeea4481bbb462c6f8f9a
Pramod Singh Gusain <spramodgusain@gmail.com>
2025-07-27 12:29:09 +0530

    perf: create functions file

--- src/functions.php
+++ src/functions.php
@@ -2,7 +2,14 @@
 declare(strict_types=1);
 namespace RestJS;
 
+use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+
+/** Query Response Function */
+function response(Request $req, Response $res, mixed $args) {
+    $res->getBody()->write(json_encode($args));
+    return $res;
+}
 
 /** Filter Function for Response Data Manipulation */
 function filter($data, Request $req): array {
